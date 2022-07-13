3078b5928a38052a3d78b80d5d46702227c05970
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-07-13 15:41:34 +1000

    add min length

--- src/scancode/dto/create-scancode.dto.ts
+++ src/scancode/dto/create-scancode.dto.ts
@@ -1,8 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsString, Length } from 'class-validator';
 
 export class CreateScancodeDto {
   @IsString()
+  @Length(6)
   @ApiProperty()
   code: string;
 }
