6faa4e1767f5100d0827e71db912b5a9cf6c8e92
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-12-03 10:34:29 +0000

    make secret optional

--- prisma/migrations/20221203103355_event_optional_secret/migration.sql
+++ prisma/migrations/20221203103355_event_optional_secret/migration.sql
@@ -2,11 +2,10 @@
   Warnings:
 
   - A unique constraint covering the columns `[secret]` on the table `Event` will be added. If there are existing duplicate values, this will fail.
-  - The required column `secret` was added to the `Event` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
 
 */
 -- AlterTable
-ALTER TABLE "Event" ADD COLUMN     "secret" TEXT NOT NULL;
+ALTER TABLE "Event" ADD COLUMN     "secret" TEXT;
 
 -- AlterTable
 ALTER TABLE "User" ADD COLUMN     "defaultStatus" "RSVPStatus" NOT NULL DEFAULT 'NO';
