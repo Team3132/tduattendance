7f038b39c6a70c49b486bfa1818967fb86064fca
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2023-01-28 10:32:41 +0000

    fix user update validation

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,14 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { RSVPStatus } from '@prisma/client';
-import { IsEnum, IsOptional, ValidateIf } from 'class-validator';
+import { IsEnum, IsOptional } from 'class-validator';
 
 /**
  * The data used to update a user
  */
 export class UpdateUserDto {
   @IsOptional()
-  @IsEnum(RSVPStatus)
-  @ValidateIf((v) => v !== null || v !== '')
+  @IsEnum({ ...RSVPStatus, unknown: null })
   @ApiProperty({ required: false, enum: RSVPStatus, nullable: true })
-  defaultStatus: RSVPStatus | null;
+  defaultStatus?: RSVPStatus | null;
 }
